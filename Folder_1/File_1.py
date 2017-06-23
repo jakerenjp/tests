8fcfa9423efa13a93c5d53eb8905e65b16e12902
Jake Ren <jakeren@gmail.com>
2017-06-23 04:10:09 +0000

    initial commit

---
+++ Folder_1/File_1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+
+for i in range(0,10,2):
+	print i
+	i+=1
+
+lst = ["apple","orange","banana"]
+
+for fruit in lst:
+	print fruit	
---
+++ Folder_1/File_2.py
@@ -0,0 +1,2 @@
+for i in range(0, 33, 3):
+	print i-1
